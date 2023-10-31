34619bbc67a733f3f6864a63e63ebe17d08fc5fb
feigi434 <feigi96@gmail.com>
2023-10-31 09:05:38 +0200

    Create Discount class

---
+++ Rebar/Models/Discount.cs
@@ -0,0 +1,30 @@
+﻿namespace Rebar.Models
+{
+    public class Discount
+    {
+        private string _name = null!;
+        private int _percent;
+
+        public string Name
+        {
+            get { return _name; } 
+            set { _name = value; }
+        }
+
+        public int Percent
+        {
+            get { return _percent; }
+            set { _percent = value; }
+        }
+
+        public Discount(string name, int percent)
+        {
+           if(name == null)
+                throw new ArgumentNullException("name");
+           this.Name = name;
+           
+            if(Enumerable.Range(1, 100).Contains(percent))
+                this.Percent = percent;
+        }
+    }
+}
