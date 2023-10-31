2e61439b65848fd2c77d81b9934aee131c63abf8
feigi434 <feigi96@gmail.com>
2023-10-31 09:05:14 +0200

    Create Account class

---
+++ Rebar/Models/Account.cs
@@ -0,0 +1,26 @@
+﻿namespace Rebar.Models
+{
+    public class Account
+    {
+        private List<Order> _orders = null!;
+        private int _sumOrders ;
+
+        public List<Order> Orders
+        {
+            get { return _orders; }
+            set { _orders = value; }
+        }
+
+        public int SumOrders
+        {
+            get { return _sumOrders; }
+            set { _sumOrders = value; }
+        }
+
+        public Account()
+        {
+            Orders = new List<Order> { };
+            SumOrders = 0;
+        }
+    }
+}
