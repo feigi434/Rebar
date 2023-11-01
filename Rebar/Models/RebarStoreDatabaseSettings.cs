58530e38bb7aa63b9e481eceaecd70bb7f57d216
feigi434 <feigi96@gmail.com>
2023-11-01 13:37:03 +0200

    Add databaseSettings

---
+++ Rebar/Models/RebarStoreDatabaseSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Rebar.Models
+{
+    public class RebarStoreDatabaseSettings : IRebarStoreDatabaseSettings
+    {
+        public string ShakeCollectionName { get; set; } = String.Empty;
+        public string OrderCollectionName { get; set; } = String.Empty;
+        public string ConnectionString { get; set; } = String.Empty;   
+        public string DatabaseName { get; set; } = String.Empty;
+    }
+}
