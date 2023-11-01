ae7bb93d7f6929de51ac6e6ccc81030145a7adba
feigi434 <feigi96@gmail.com>
2023-11-01 13:35:59 +0200

    Add conntrolers

---
+++ Rebar/Controllers/OrdersController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rebar.Models;
+using Rebar.Services;
+
+namespace Rebar.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IOrderService orderService;
+        public OrdersController(IOrderService orderService)
+        {
+            this.orderService = orderService;
+        }
+
+        // GET: api/<OrdersController>
+        [HttpGet]
+        public ActionResult<List<Order>> Get()
+        {
+            return orderService.Get();
+        }
+
+        // GET api/<OrdersController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Order> Get(Guid id)
+        {
+            var order = orderService.Get(id);
+
+            if(order == null)
+            {
+                return NotFound($"Order with id = {id} not found");
+            }
+
+            return order;
+        }
+
+        // POST api/<OrdersController>
+        [HttpPost]
+        public ActionResult<Order> Post([FromBody] Order order)
+        {
+            orderService.Create(order);
+
+            return CreatedAtAction(nameof(Get), new { id = order.Id }, order);
+        }
+
+
+        // PUT api/<OrdersController>/5
+        [HttpPut("{id}")]
+        public ActionResult Put(Guid id, [FromBody] Order order)
+        {
+            var existingOrder = orderService.Get(id);
+
+            if(existingOrder == null)
+            {
+                return NotFound($"Order with id = {id} not found");
+            }
+
+            orderService.Update(id, order);
+
+            return NoContent();
+        }
+
+        // DELETE api/<OrdersController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(Guid id)
+        {
+            var order = orderService.Get(id);
+
+            if (order == null)
+            {
+                return NotFound($"Order with id = {id} not found");
+            }
+
+            orderService.Remove(order.Id);
+
+            return Ok($"Order with id = {id} deleted");
+        }
+
+         
+    }
+}
---
+++ Rebar/Controllers/ShakesController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rebar.Models;
+using Rebar.Services;
+
+namespace Rebar.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShakesController : ControllerBase
+    {
+        private readonly IShakeService shakeService;
+        public ShakesController(IShakeService shakeService)
+        {
+            this.shakeService = shakeService;
+        }
+
+        // GET: api/<ShakesController>
+        [HttpGet]
+        public ActionResult<List<Shake>> Get()
+        {
+            return shakeService.Get();
+        }
+
+        // GET api/<ShakesController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Shake> Get(Guid id)
+        {
+            var shake = shakeService.Get(id);
+
+            if (shake == null)
+            {
+                return NotFound($"Shake with id = {id} not found");
+            }
+
+            return shake;
+        }
+
+        // POST api/<ShakesController>
+        [HttpPost]
+        public ActionResult<Shake> Post([FromBody] Shake shake)
+        {
+            shakeService.Create(shake);
+
+            return CreatedAtAction(nameof(Get), new { id = shake.Id }, shake);
+        }
+
+        // PUT api/<ShakesController>/5
+        [HttpPut("{id}")]
+        public ActionResult Put(Guid id, [FromBody] Shake shake)
+        {
+            var existingShake = shakeService.Get(id);
+
+            if (existingShake == null)
+            {
+                return NotFound($"Shake with id = {id} not found");
+            }
+
+            shakeService.Update(id, shake);
+
+            return NoContent();
+        }
+
+        // DELETE api/<ShakesController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(Guid id)
+        {
+            var shake = shakeService.Get(id);
+
+            if (shake == null)
+            {
+                return NotFound($"Shake with id = {id} not found");
+            }
+
+            shakeService.Remove(shake.Id);
+
+            return Ok($"Shake with id = {id} deleted");
+        }
+    }
+}
