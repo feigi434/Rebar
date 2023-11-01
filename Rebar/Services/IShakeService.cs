382924d0c82a351ee89be19e1b027337521f755a
feigi434 <feigi96@gmail.com>
2023-11-01 13:36:26 +0200

    Add services

---
+++ Rebar/Services/IShakeService.cs
@@ -0,0 +1,13 @@
+﻿using Rebar.Models;
+
+namespace Rebar.Services
+{
+    public interface IShakeService
+    {
+        List<Shake> Get();
+        Shake Get(Guid id);
+        Shake Create(Shake shake);
+        void Update(Guid id, Shake shake);
+        void Remove(Guid id);
+    }
+}
---
+++ Rebar/Services/ShakeService.cs
@@ -0,0 +1,42 @@
+﻿using Rebar.Models;
+using MongoDB.Driver;
+
+namespace Rebar.Services
+{
+    public class ShakeService : IShakeService
+    {
+        private readonly IMongoCollection<Shake> _shakes;
+
+        public ShakeService(IRebarStoreDatabaseSettings settings, IMongoClient mongoClient)
+        {
+            var database = mongoClient.GetDatabase(settings.DatabaseName);
+            _shakes = database.GetCollection<Shake>(settings.ShakeCollectionName);
+        }
+
+        Shake IShakeService.Create(Shake shake)
+        {
+            _shakes.InsertOne(shake);
+            return shake;
+        }
+
+        List<Shake> IShakeService.Get()
+        {
+            return _shakes.Find(shake => true).ToList();
+        }
+
+        Shake IShakeService.Get(Guid id)
+        {
+            return _shakes.Find(shake => shake.Id == id).FirstOrDefault();
+        }
+
+        public void Remove(Guid id)
+        {
+            _shakes.DeleteOne(shake => shake.Id == id);
+        }
+
+        void IShakeService.Update(Guid id, Shake shake)
+        {
+            _shakes.ReplaceOne(shake => shake.Id == id, shake);
+        }
+    }
+}
